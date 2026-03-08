4b4299b7d45fac42ff634ed0405916d0098aa595
ferris4096 <charbons.82ions@icloud.com>
2026-03-08 17:47:27 +0100

    Fixed layout a bit

--- src/ui.rs
+++ src/ui.rs
@@ -17,23 +17,21 @@ impl Widget for &App {
     // - https://docs.rs/ratatui/latest/ratatui/widgets/index.html
     // - https://github.com/ratatui/ratatui/tree/master/examples
     fn render(self, area: Rect, buf: &mut Buffer) {
-        if let Some(location) = &self.location
-            && let Some(proto) = &self.cur_frame
-        {
+        if let Some(proto) = &self.cur_frame {
             let image = Image::new(proto);
             image.render(area, buf);
+        }
 
-            let layout = Layout::default()
-                .direction(Direction::Vertical)
-                .constraints([
-                    Constraint::Length(5),
-                    Constraint::Length(3),
-                    Constraint::Min(0),
-                ])
-                .split(area);
-
+        if let Some(location) = &self.location {
             let content = format!("{}, {}", location.neighborhood, location.country);
             let content_width = content.len() as u16;
+            let content_rect = if area.width > 92 {
+                Rect::new(0, 0, area.width, 5)
+                    .centered_horizontally(Constraint::Max(content_width + 4))
+            } else {
+                Rect::new(0, 3, area.width, 3)
+                    .centered_horizontally(Constraint::Max(content_width + 4))
+            };
 
             let town_name = Paragraph::new(content)
                 .style(Style::default().bg(Color::Rgb(0, 132, 48)).fg(Color::White))
@@ -42,42 +40,36 @@ impl Widget for &App {
                 .block(
                     Block::bordered()
                         .border_type(BorderType::Rounded)
-                        .padding(Padding::vertical(1)),
+                        .padding(Padding::vertical(if area.width > 92 { 1 } else { 0 })),
                 );
 
-            Clear.render(
-                layout[0].centered_horizontally(Constraint::Max(content_width + 4)),
-                buf,
-            );
+            Clear.render(content_rect, buf);
 
-            town_name.render(
-                layout[0].centered_horizontally(Constraint::Max(content_width + 4)),
-                buf,
-            );
+            town_name.render(content_rect, buf);
+
+            let content_rect = Rect::new(0, content_rect.bottom(), area.width, 3)
+                .centered_horizontally(Constraint::Max(location.road.len() as u16 + 2));
 
             let street_name = Paragraph::new(location.road.clone())
                 .style(Style::default().bg(Color::White).fg(Color::Black))
                 .centered()
                 .block(Block::bordered().border_type(BorderType::Rounded));
-            street_name.render(
-                layout[1].centered_horizontally(Constraint::Max(location.road.len() as u16 + 2)),
-                buf,
-            );
-
-            let content: Line = vec![
-                "● ".red(),
-                format!("{} drivers online", self.users_online).black(),
-            ]
-            .into();
-            let content_width = content.width() as u16 + 4;
-            let content_rect = Rect::new(area.width - content_width, 0, content_width, 3);
-            Clear.render(content_rect, buf);
-            let drivers_online = Paragraph::new(content)
-                .style(Style::default().bg(Color::Rgb(255, 242, 2)))
-                .centered()
-                .block(Block::bordered().border_type(BorderType::Rounded).black())
-                .bold();
-            drivers_online.render(content_rect, buf);
+            street_name.render(content_rect, buf);
         }
+
+        let content: Line = vec![
+            "● ".red(),
+            format!("{} drivers online", self.users_online).black(),
+        ]
+        .into();
+        let content_width = content.width() as u16 + 4;
+        let content_rect = Rect::new(area.width - content_width, 0, content_width, 3);
+        Clear.render(content_rect, buf);
+        let drivers_online = Paragraph::new(content)
+            .style(Style::default().bg(Color::Rgb(255, 242, 2)))
+            .centered()
+            .block(Block::bordered().border_type(BorderType::Rounded).black())
+            .bold();
+        drivers_online.render(content_rect, buf);
     }
 }
